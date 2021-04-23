7bd6e077a1074f2dbe18411b38e80f2b1a7748cd
Ristirianto Adi <ristiriantoadi@gmail.com>
2021-04-23 16:51:21 +0800

    create user done

--- user/views.py
+++ user/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseNotFound, Http404
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def login_view(request,*args, **kwargs):
     raise Http404("there is no login page--login via popup")
 
 def register_view(request,*args, **kwargs):
-    raise Http404("there is no register page--register via popup")
+    if(request.method == "POST"):
+        form = UserCreationForm(request.POST)
+        print(form)
+        if form.is_valid():
+            form.save()
+            return redirect("stories")
+        else:
+            print("error")
+        # print(request.POST)#request.POST.get('title') or 
+    # raise Http404("there is no register page--register via popup")
 
 def user_profile_view(request,username):
     context = {
